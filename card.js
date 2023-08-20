b64a5414c7fbb3a8faf8b64870caf0200a956600
ali8700 <alirhouma870@gmail.com>
2023-08-20 10:05:42 +0200

    first commit pour notre page web

---
+++ card.js
@@ -0,0 +1,36 @@
+const buttonMinus=document.querySelectorAll(".minus");
+const PlusButton=document.querySelectorAll(".plus");
+const totalSum=document.querySelector("#sum");
+const deleteButton=document.querySelectorAll(".btn-primary");
+const quantitesButton=document.querySelectorAll(".x");
+const prices=document.querySelectorAll(".price");
+const bigDivs= document.querySelectorAll(".card");
+for(let i=0;i<buttonMinus.length;i++){
+    buttonMinus[i].addEventListener("click",function(e){
+   if (quantitesButton[i].innerHTML>0){
+    quantitesButton[i].innerHTML--;
+    totalSum.textContent =
+    parseInt(total.textSum.textContent)-parseInt(prices[i].innerHTML)
+
+   }
+    });
+}
+  for(let i=0;i< buttonMinus.length; i++) {
+  PlusButton[i].addEventListener("click",function(e){
+    quantitesButton[i].innerHTML++;
+    totalSum.textContent=
+    parseInt(totalSum.textContent)+parseInt(prices[i].innerHTML);
+  });
+
+  }
+  for(let i =0; i<deleteButton.length; i++){
+    deleteButton[i].addEventListener("click",function(e){
+     bigDivs[i].remove();
+     totalSum.textContent =
+     parseInt(totalSum.textContent) -
+     parseInt(quantitesButton[i].textContent)*
+     parseInt(prices[i].textContent);
+    });
+} 
+
+  
